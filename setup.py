5b1799430bf551e9a1a5367f12e2fc83cb0c01f3
hbrendan <hbrendan@umich.edu>
2019-02-11 23:41:29 -0500

    static files

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
+    package_data = {
+        'templates': ['*'],
+    },
     name="linestar-scrape",
-    version="0.0.2",
+    version="0.0.3",
     author="Brendan Hart",
     author_email="brendanahart@gmail.com",
-    description="A package to scrape linestarapp.com ownership data",
+    description="A package to scrape linestarapp.com daily fantasy sports ownership data",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/WolverineSportsAnalytics/linestar-scrape",
